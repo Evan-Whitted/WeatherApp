7da513d960979d19712491f1ce38553afb891b32
evan-whitted <evanwhitted@gmail.com>
2023-01-26 15:04:58 -0500

    added the new colors and background

---
+++ src/app/Services/weather.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment.ts/environment';
+import { WeatherData } from '../Models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>('https://bestweather.p.rapidapi.com/weather/' + encodeURI(cityName) + '/today', {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('unitGroup', 'us')
+    })
+  }
+}
+
---
+++ src/environment.ts/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://bestweather.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'bestweather.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '6f61232876mshe0451d5a29d1e61p1ab866jsn3bc3bcb465b5',
+
+
+}
