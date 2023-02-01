951f1630745158697ec20722a7a5b9c1e4f4df66
evan-whitted <evanwhitted@gmail.com>
2023-02-01 15:18:11 -0500

    updated weather service

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -27,7 +27,7 @@ API_KEY = '656d825f90356ea3ea9a6e46f9d0603d';
          this.latitude = success.coords.latitude; 
           this.longitude = success.coords.longitude;
         return this.http.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&units=Imperial&appid=${this.API_KEY}`);
-  })
+  })    
     return this.http.get<WeatherData>(`https://api.openweathermap.org/data/2.5/weather?lat=${this.latitude}&lon=${this.longitude}&units=Imperial&appid=656d825f90356ea3ea9a6e46f9d0603d`); 
   
   
