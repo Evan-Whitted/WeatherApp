578f039291295014d7026efa614a91abc33e385b
JackBruzan <jackbruzan@gmail.com>
2023-02-01 18:03:18 -0600

    added home page moved stuff out of app component more?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherData } from './Models/weather.model';
-import { WeatherService } from './Services/weather.service';
-import { GeoCode } from './Models/geocode.model';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  cityName: string = 'Chicago'
-  weatherData?: WeatherData;
-  geoCodes?: GeoCode[] = [];
+export class AppComponent {
+  title = 'my pbl crashboard';
 
-
-  constructor(private weatherService: WeatherService) {
-
-  }
-
-  ngOnInit(): void {
-    this.getWeatherDataFromCurrentLocation(this.cityName);
-    this.cityName = '';
-
-  };
-
-  onSubmit() {
-    this.getWeatherDataFromSearch(this.cityName);
-  }
-
-  private getWeatherDataFromCurrentLocation(cityName: string) {
-    this.weatherService.getWeatherDataForCurrentLocation()
-      .subscribe({
-        next: (response) => {
-          this.weatherData = response;
-
-          //console.log(response);
-        }
-      })
-  }
-
-  getWeatherDataFromSearch(cityName: string) {
-    this.weatherService.getGeoCodeForCityName(cityName).subscribe(gc => {
-      this.geoCodes = gc;
-      this.getWeatherFromLatLong(gc[0].lat.toString(), gc[0].lon.toString());
-    })
-  }
-
-
-
-
-  getWeatherFromLatLong(latFromGeoCodeAPI: string, longFromGeoCodeAPI: string): void {
-    this.weatherService.getWeatherDataForLatLong(latFromGeoCodeAPI, longFromGeoCodeAPI).subscribe(wd => {
-      this.weatherData = wd;
-    })
-  }
+  constructor(
+    private router: Router,
+  ) { }
 }
